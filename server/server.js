d3c391d2316eca29611ea58ec5b80d58fddb0752
Jackson Fiore <jrfiore@tx.local>
2017-05-10 11:33:41 -0400

    add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Jackson',
+    text: 'Hey there buddy',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
@@ -21,7 +31,7 @@ io.on('connection', (socket) => {
 
 server.listen(port, () => {
   console.log(`Server started on port ${port}`);
-})
+});
 
 
 // console.log(__dirname + '/../public');
